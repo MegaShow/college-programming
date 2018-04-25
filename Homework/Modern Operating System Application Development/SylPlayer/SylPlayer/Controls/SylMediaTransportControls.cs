659560a84ae9cc7a0fccdb7d746df9c6dbc348ae
MegaShow <MegaXiu@outlook.com>
2018-04-25 21:46:59 +0800

    2018.4.25

---
+++ Homework/Modern Operating System Application Development/SylPlayer/SylPlayer/Controls/SylMediaTransportControls.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SylPlayer.Controls {
+    public sealed class SylMediaTransportControls : MediaTransportControls {
+        public event EventHandler<EventArgs> FileOpenClick;
+        public event EventHandler<EventArgs> PlayPauseClick;
+
+        public SylMediaTransportControls() {
+            this.DefaultStyleKey = typeof(SylMediaTransportControls);
+        }
+
+        protected override void OnApplyTemplate() {
+            var fileButton = GetTemplateChild("FileButton") as Button;
+            var playPauseButton = GetTemplateChild("PlayPauseButton") as Button;
+            fileButton.Click += FileButton_Click;
+            playPauseButton.Click += PlayPauseButton_Click;
+            base.OnApplyTemplate();
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e) {
+            PlayPauseClick?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void FileButton_Click(object sender, RoutedEventArgs e) {
+            FileOpenClick?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Homework/Modern Operating System Application Development/SylPlayer/SylPlayer/Views/MediaPlayerPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.Core;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Pickers;
+using Windows.System.Display;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace SylPlayer.Views {
+    public sealed partial class MediaPlayerPage : Page, INotifyPropertyChanged {
+        private DisplayRequest _displayRequest = new DisplayRequest();
+        private bool _isRequestActive = false;
+        private bool _isPlay = false;
+
+        public MediaPlayerPage() {
+            InitializeComponent();
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+            mpe.MediaPlayer.PlaybackSession.PlaybackStateChanged += PlaybackSession_PlaybackStateChanged;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e) {
+            base.OnNavigatedFrom(e);
+            mpe.MediaPlayer.Pause();
+            mpe.MediaPlayer.PlaybackSession.PlaybackStateChanged -= PlaybackSession_PlaybackStateChanged;
+        }
+
+        private async void PlaybackSession_PlaybackStateChanged(MediaPlaybackSession sender, object args) {
+            if (sender is MediaPlaybackSession playbackSession && playbackSession.NaturalVideoHeight != 0) {
+                if (playbackSession.PlaybackState == MediaPlaybackState.Playing) {
+                    if (!_isRequestActive) {
+                        await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                            _displayRequest.RequestActive();
+                            _isRequestActive = true;
+                        });
+                    }
+                } else {
+                    if (_isRequestActive) {
+                        await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                            _displayRequest.RequestRelease();
+                            _isRequestActive = false;
+                        });
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null) {
+            if (Equals(storage, value)) {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private async void SylMediaTransportControls_FileOpenClick(object sender, EventArgs e) {
+            var openPicker = new FileOpenPicker();
+            openPicker.FileTypeFilter.Add(".wmv");
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".wma");
+            openPicker.FileTypeFilter.Add(".mp3");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null) {
+                pictureRotate.Stop();
+                mpe.Source = MediaSource.CreateFromStorageFile(file);
+                mpe.MediaPlayer.Play();
+                _isPlay = true;
+                if (mpe.MediaPlayer.PlaybackSession.NaturalVideoHeight == 0) {
+                    grid.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                    StorageItemThumbnail thumbnail = await file.GetThumbnailAsync(ThumbnailMode.SingleItem);
+                    if (thumbnail == null) {
+                        pictureImage.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/music.png"));
+                    } else {
+                        BitmapImage image = new BitmapImage();
+                        image.SetSource(thumbnail);
+                        pictureImage.ImageSource = image;
+                    }
+                    pictureRotate.Begin();
+                } else {
+                    grid.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                }
+            }
+        }
+
+        private void SylMediaTransportControls_PlayPauseClick(object sender, EventArgs e) {
+            if (grid.Visibility == Windows.UI.Xaml.Visibility.Visible) {
+                if (_isPlay) {
+                    _isPlay = false;
+                    pictureRotate.Pause();
+                } else {
+                    _isPlay = true;
+                    pictureRotate.Resume();
+                }
+            }
+        }
+    }
+}
